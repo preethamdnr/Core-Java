93df0ef98605a7f74b8906ce5688990f1dbf6e85
preethamdnr <preethamdongari@gmail.com>
2024-09-18 05:23:56 -0500

    Core Java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String args[]){
+        int a=5;
+        int b=10;
+        int result=a+b;
+        System.out.println("Hello World, I am Preetham Dongari");
+        System.out.println(result);
+}
+}
